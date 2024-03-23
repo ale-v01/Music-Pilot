caa5dd528f9c4ca6e0e92524d8046c3697df700e
Alejandro Villalobos <alev111201@gmail.com>
2024-03-23 18:33:57 -0500

    fixed variable in microphone.py to resemble dynamic

--- microphone.py
+++ microphone.py
@@ -7,9 +7,9 @@ CHUNK = 1024  # Number of audio frames per buffer
 FORMAT = pyaudio.paInt16  # Audio format (16-bit PCM)
 CHANNELS = 1  # Single channel for microphone
 RATE = 44100  # Sample rate
-RECORD_SECONDS = int(input("Length of recording in seconds: "))  # Duration of recording
-FILENAME = input("Output file name: ")  # Output filename
-WAVE_OUTPUT_FILENAME = f"audio_files/{FILENAME}.wav"
+record_seconds = int(input("Length of recording in seconds: "))  # Duration of recording
+filename = input("Output file name: ")  # Output filename
+wave_output_filename = f"audio_files/{filename}.wav"
 
 p = pyaudio.PyAudio()
 
@@ -24,7 +24,7 @@ print("* recording")
 
 frames = []
 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+for i in range(0, int(RATE / CHUNK * record_seconds)):
     data = stream.read(CHUNK)
     frames.append(data)
 
@@ -37,7 +37,7 @@ stream.close()
 p.terminate()
 
 # Save the recorded data as a WAV file
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf = wave.open(wave_output_filename, 'wb')
 wf.setnchannels(CHANNELS)
 wf.setsampwidth(p.get_sample_size(FORMAT))
 wf.setframerate(RATE)
